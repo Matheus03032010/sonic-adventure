167a2e51ff8853217f9215a71ae430eb774c7c48
edwedja <edwedja_lima@hotmail.com>
2022-08-04 17:27:54 -0300

    novas imagens

--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
 var sonic
-var sonicanimation
+var sonicanimation;
+var sonicpulo;
+var solo
 
 function preload(){ // função que carregar todas as imagens e animações
   sonicanimation = loadAnimation ("assets/s1.png","assets/s2.png","assets/s3.png","assets/s4.png")
+  sonicpulo = loadAnimation("assets/p1.png","assets/p2.png", "assets/p3.png","assets/p4.png","assets/p5.png")
 }
 
 function setup(){ // todas as configuraçoes dos objetos
   createCanvas(windowWidth,windowHeight);
   sonic = createSprite(500,600)
   sonic.addAnimation("sonic",sonicanimation )
+  sonic.addAnimation("sonicpulo",sonicpulo )
+
+  solo = createSprite(windowWidth/2, windowHeight-200, width, 20)
  
   }
   
@@ -16,11 +22,32 @@ function setup(){ // todas as configuraçoes dos objetos
 function draw(){
   background("black");
   drawSprites(); 
-  if(keyIsDown(RIGHT_ARROW)){
-    sonic.x+= 10
-  }   
-  if(keyIsDown(LEFT_ARROW)){
-    sonic.x-= 10
-  }
+  controle()
+  
+}
+
+
+  function controle(){
+    if(keyIsDown(RIGHT_ARROW)){
+      sonic.x+= 10
+    }   
+    if(keyIsDown(LEFT_ARROW)){
+      sonic.x-= 10
+    }
+  
+    sonic.velocityY += 0.8
+     if(keyDown("space") && sonic.y>(windowHeight-400)){
+      sonic.velocityY= -10
+      sonic.changeAnimation("sonicpulo", sonicpulo)
+      sonic.scale = 1.2
+    }
+  
+    if(sonic.collide(solo)){
+      sonic.changeAnimation("sonic", sonicanimation)
+      sonic.scale = 1
+    }
   }
 
+  function inimigos(){
+    
+  }
