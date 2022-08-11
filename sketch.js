27620a5967bbaae7c162d2ae1d98c6f94c23a9c5
edwedja <edwedja_lima@hotmail.com>
2022-08-11 14:37:21 -0300

    aula 48 inicio

--- sketch.js
+++ sketch.js
@@ -4,9 +4,10 @@ var sonicpulo, sonicleft;
 var solo;
 var robo, inimigo_anm, inimigo;
 var estado = "jogar";
-var b1
+var b1;
 var b2;
-var grupob
+var grupob, grupocoin, coinimg, grupoin;
+var score=0
 function preload(){ // função que carregar todas as imagens e animações
   sonicanimation = loadAnimation ("assets/s1.png","assets/s2.png","assets/s3.png","assets/s4.png")
   sonicpulo = loadAnimation("assets/p1.png","assets/p2.png", "assets/p3.png","assets/p4.png","assets/p5.png")
@@ -14,11 +15,13 @@ function preload(){ // função que carregar todas as imagens e animações
   inimigo_anm = loadAnimation ("assets/i6.png","assets/i7.png","assets/i8.png","assets/i9.png","assets/i10.png","assets/i11.png","assets/i12.png","assets/i13.png")
   b1 = loadAnimation("assets/b1.png")
   b2 = loadAnimation("assets/b2.png")
-  solo
+  coinimg = loadImage ("assets/coin.png")
 }
 
 function setup(){ // todas as configuraçoes dos objetos
   grupob=new Group() 
+  grupocoin= new Group()
+  grupoin = new Group()
   
   createCanvas(windowWidth,windowHeight);
   sonic = createSprite(500,600)
@@ -26,11 +29,11 @@ function setup(){ // todas as configuraçoes dos objetos
   sonic.addAnimation("sonicpulo",sonicpulo )
   sonic.addAnimation("sonicleft",sonicleft )
 
-  robo = createSprite(200, 600)
-  robo.addAnimation("robo")
+  // robo = createSprite(200, 600)
+  // robo.addAnimation("robo")
   solo = createSprite(windowWidth/2, windowHeight-200, width, 20)
 
-  inimigo = createSprite(50,600,20,20)
+  inimigo = createSprite(10,580,20,20)
   inimigo.addAnimation("in",inimigo_anm)
   inimigo.scale = 1.5
   }
@@ -44,7 +47,18 @@ function draw(){
     controle()
      inimigos()
      blocos()
+      moedas()
      sonic.collide(grupob)
+
+     text("SCORE: "+score, sonic.x+200, sonic.y-100)
+     sonic.overlap(grupocoin, function (collector, collected){
+      score ++
+      collected.remove()
+     })
+     sonic.overlap(grupoin, function (collector, collected){
+      score --
+      collected.remove()
+     })
   }else if (estado === "encerrar"){
 
   }
@@ -78,12 +92,13 @@ function draw(){
   }
 
   function inimigos(){
-    if(frameCount%60===0){
-    var robos = createSprite(sonic.x+800,sonic.y,20,20)
+    if(frameCount%200===0){
+    var robos = createSprite(sonic.x+800,sonic.y-60,20,20)
     robos.lifetime = 1000
     robos.addAnimation("robo", b2) 
     robos.scale = 0.5
-    //robos.velocityX =-3
+    robos.velocityX =-3
+     grupoin.add(robos)
     }
   }
 
@@ -94,5 +109,15 @@ function draw(){
     blocos.addAnimation("blocos", b1) 
     blocos.scale = 0.5
     grupob.add(blocos)
+    }
+  }
+
+  function moedas(){
+    if(frameCount%100===0){
+    var coin= createSprite(sonic.x+800,sonic.y,20,20)
+    coin.lifetime = 1000
+    coin.addAnimation("coin", coinimg) 
+    coin.scale = 0.2
+    grupocoin.add(coin)
     }
   }
